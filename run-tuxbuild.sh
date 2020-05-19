roxell/tuxbuild-wrappers

commit 6d3938327a174796e5bc97791b074ab8c7533b79

run-tuxbuild: tuxbuild: update from git-ref to git-sha
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

run-tuxbuild.sh
@@ -98,7 +98,7 @@ OUTPUTDIR=${TOP}/$(date +"%Y%m%d-%H%m")
 mkdir -p ${OUTPUTDIR}
 tb_json_artifact="${OUTPUTDIR}/build-artifact.json"
 logfilename=$(echo $(basename ${FILE})|awk -F. '{print $1}').log
-echo tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-ref ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} | tee ${OUTPUTDIR}/${logfilename}
+echo tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-sha ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} | tee ${OUTPUTDIR}/${logfilename}
 tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-ref ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} 2>&1 | tee -a ${OUTPUTDIR}/${logfilename}
 
 if [[ ${RANDCONFIG} -eq 1 ]]; then

commit 9aebfac0b89cd3d9cf9867b14cc2f2145a2e213c

run-tuxbuild: fix stupidity
Fixes: 6d3938327a17 ("run-tuxbuild: tuxbuild: update from git-ref to git-sha")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

run-tuxbuild.sh
@@ -100,7 +100,7 @@ mkdir -p ${OUTPUTDIR}
 tb_json_artifact="${OUTPUTDIR}/build-artifact.json"
 logfilename=$(echo $(basename ${FILE})|awk -F. '{print $1}').log
 echo tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-sha ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} | tee ${OUTPUTDIR}/${logfilename}
-tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-ref ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} 2>&1 | tee -a ${OUTPUTDIR}/${logfilename}
+tuxbuild build-set --git-repo ${GIT_REPOSITORY} --git-sha ${BRANCH} --tux-config ${FILE} --set-name basic --json-out ${tb_json_artifact} 2>&1 | tee -a ${OUTPUTDIR}/${logfilename}
 
 if [[ ${RANDCONFIG} -eq 1 ]]; then
 	file_list="${DEFAULT_FILE_LIST}"